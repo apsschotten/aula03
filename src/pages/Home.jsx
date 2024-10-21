d9f23843c2334e4bf6ea2bd76bea4501f61c9be4
xoten herself <ana.schotten@estudante.ifms.edu.br>
2024-10-21 18:22:08 -0300

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,8 @@ export default function Home() {
                 const dados = await resposta.json();
                 setLista(dados)
             } catch (error) {
-                alert("Falha durante a comunicação com o servidor."); //Modo 01 (Sala de Aula)
-                console.error("Falha durante a comunicação com o servidor.") //Modo 02 (Pedido em Moodle)
+                alert("Falha durante a comunicação com o servidor."); //Modo 01 (Sala de Aula) --> Alerta diretamente o usuário
+                console.error("Falha durante a comunicação com o servidor.") //Modo 02 (Pedido em Moodle) --> Escreve a mensagem de erro no console            
             }
         }
         receberListaProdutos();
