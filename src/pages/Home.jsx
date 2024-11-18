56737c90078f46c8f4248d3382e8ca09de85d6ff
apsschotten <ana.schotten@estudante.ifms.edu.br>
2024-11-18 15:47:57 -0400

    .

---
+++ src/pages/Home.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import styles from '../styles/Home.module.css'
+
+
+export default function App() {
+
+    const [lista, setLista] = useState([]);
+
+    useEffect(() => {
+        const receberListaProdutos = async () => {
+            const resposta = await fetch('https://fakestoreapi.com/products');
+            const dados = await resposta.json();
+            setLista(dados);
+        }
+
+        receberListaProdutos();
+    }, []);
+
+    if (lista.length === 0) {
+        return (
+            <h1>Carregando...</h1>
+        )
+    }
+
+    const orderAZ = () => {
+        const listaAux = [...lista].sort((a, b) => a.title.localeCompare(b.title));
+        setLista(listaAux);
+    }
+
+    return (
+        <>
+            <header className={styles.header}>
+                <h1 className={styles.logo}>Lolja</h1>
+            </header>
+            <button onClick={orderAZ}>A - Z</button>
+            <div className={styles.container}>
+                {lista.map(produto => (
+                    <div className={styles.cardscontainer}>
+                        <div className={styles.card}>
+                            <img src={produto.image} alt={produto.title} className={styles.image} />
+                            <div className={styles.content}>
+                                <h2 className={styles.title}>{produto.title}</h2>
+                                <p className={styles.description}>{produto.description}</p>
+                                <p className={styles.price}>R$ {produto.price.toFixed(2)}</p>
+                                <a href="#" className={styles.button}>Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
